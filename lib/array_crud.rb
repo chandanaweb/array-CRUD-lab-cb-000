92c447861504549b493068a92f3cc132a2b606ab
chandana M <chandu11288@gmail.com>
2018-02-08 19:50:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,33 +4,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-arr = [1,2,3,4]
+ arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-array.push(element)
+ array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-array.unshift(element)
+ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-array.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-array.shift
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-return array[index_number]
+ return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-array.first
+ array.first
 end
 
 def retrieve_last_element_from_array(array)
-array.last
+ array.last
 end
